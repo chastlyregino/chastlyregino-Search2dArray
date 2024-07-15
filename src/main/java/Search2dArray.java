32f52e6e9c49429829b4a5288f55a65675cfc711
Finn Chastly Manapat <chastly_regino@icloud.com>
2024-07-15 20:15:21 +0000

    finding a target parameter in a 2d array

--- src/main/java/Search2dArray.java
+++ src/main/java/Search2dArray.java
@@ -10,6 +10,14 @@ public class Search2dArray {
      * @return true if nums contains target, and false otherwise.
      */
     public boolean search(int[][] nums, int target){
+
+        for (int[] numberArray : nums) { //first array
+            for (int number : numberArray) { //second array
+                if ( number == target ) { //check if target is in nums[][]
+                    return true; //early return;
+                }
+            }
+        }
         return false;
     }
 }
